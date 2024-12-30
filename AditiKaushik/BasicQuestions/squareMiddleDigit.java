55a28fc26136b10e49694afaf4810ec47f97a58a
Aditi-Kaushik04 <aditikaushik6279.11e@gmail.com>
2024-12-30 14:53:53 +0530

    Adding 3 digit Questions

--- AditiKaushik/BasicQuestions/squareMiddleDigit.java
+++ AditiKaushik/BasicQuestions/squareMiddleDigit.java
@@ -2,15 +2,14 @@ package AditiKaushik.BasicQuestions;
 
 import java.util.Scanner;
 
-class sumOfDigits {
+class squareMiddleDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a 3-digit number: ");
         int number = sc.nextInt();
         if (number >= 100 && number <= 999) {
-            int firstDigit = number / 100;
-            int lastDigit = number % 10;
-            System.out.println("The sum of the 1st and 3rd digit is: " + (firstDigit + lastDigit));
+            int middleDigit = (number / 10) % 10;
+            System.out.println("The square of the middle digit is: " + (middleDigit * middleDigit));
         }
         else {
             System.out.println("The number entered is not a 3-digit number.");
