18086fec495c4ca0d292a85bb626e73717343701
PoojaKumari-01 <pooja.2k25@gmail.com>
2025-01-04 12:08:43 +0530

    practice test problem1,problem2,problem3

---
+++ PoojaKumari/PrefixAndSuffixSumAssesment/Problem2.java
@@ -0,0 +1,34 @@
+//package GU-Technical-Training-2026.PoojaKumari.PrefixAndSuffixSumAssesment;
+import java.util.*;
+
+
+public class Problem2 {
+    public static int calculateTotalReservedSeats(int[] booking,int[] result) {
+    int startSeat=0;
+    int endSeat=0;
+    for(int i=0;i<booking.length;i++){
+        result[i]=seats
+    }
+        
+    return 0;
+         
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Statring seats range of flights");
+        int start=sc.nextInt();
+        System.out.println("Enter end  seat Range of flights:");
+        int end=sc.nextInt();
+        System.out.pritnln("Enter number of seats:");
+        int seats=sc.nextInt();
+        int[] booking={start,end,seats};
+        int[] result=new int[booking.length];
+        for(int i=0;i<booking.length;i++){
+            //if(iscalculateTotalReservation()
+        }
+
+    }
+    
+}
+// time complexity =O(n)
+//space complexity=O(1)
