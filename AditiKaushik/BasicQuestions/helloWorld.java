d5d8b92d5c6ff6f48798d9e04967fd592624e1df
Aditi-Kaushik04 <aditikaushik6279.11e@gmail.com>
2025-01-09 12:37:57 +0530

    Adding question description and complexity

--- AditiKaushik/BasicQuestions/helloWorld.java
+++ AditiKaushik/BasicQuestions/helloWorld.java
@@ -1,7 +1,10 @@
-package AditiKaushik.BasicQuestions;
+//WAP to print Hello World.
 
 public class helloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
+
+//Time Complexity: O(1)
+//Space Complexity: O(1)
--- AditiKaushik/BasicQuestions/netAmount.java
+++ AditiKaushik/BasicQuestions/netAmount.java
@@ -1,14 +1,9 @@
-package AditiKaushik.BasicQuestions;
+//WAP to take input rate & quantity and calculate the amount. Finally print net amount to be paid after allowing 15% discount if amount exceeds 2000 rs.
 
 import java.util.Scanner;
 
 class netAmount {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter rate: ");
-        double rate = sc.nextDouble();
-        System.out.print("Enter quantity: ");
-        int quantity = sc.nextInt();
+    public static void amountPaid(double rate, int quantity){
         double amount = rate * quantity;
         double discount = 0;
         if (amount > 2000) {
@@ -20,6 +15,17 @@ class netAmount {
             System.out.println("Discount (15%): Rs. " + discount);
         }
         System.out.println("Net Amount to be Paid: Rs. " + netAmount);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter rate: ");
+        double rate = sc.nextDouble();
+        System.out.print("Enter quantity: ");
+        int quantity = sc.nextInt();
+        amountPaid(rate, quantity);
         sc.close();
     }
 }
+
+//Time Complexity: O(1)
+//Space Complexity: O(1)
