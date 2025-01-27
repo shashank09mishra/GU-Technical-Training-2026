a82ddd30c72fb88c7494095b1ec0d5c4ca7b36f7
Anya Seth <138517164+anyaaseth08@users.noreply.github.com>
2025-01-27 23:47:40 +0530

    Create Alternating Groups II.java

---
+++ Anya_Seth/Sliding Window/Alternating Groups II.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int answer = 0;
+        int count = 0;
+        int length = colors.length;
+
+        for (int i = 1, start = 0; i < 2 * length - 1; i++) {
+            if (i == length - 1) {
+                count = answer;
+            }
+            if (colors[i % length] == 1 && colors[(i - 1) % length] == 0) {
+                if (i - start + 1 >= k) {
+                    ++answer;
+                }
+            }
+            else if (colors[i % length] == 0 && colors[(i - 1) % length] == 1) {
+                if (i - start + 1 >= k) {
+                    ++answer;
+                }
+            }
+            else {
+                start = i;
+            }
+        }
+       return answer - count;
+    }
+}
