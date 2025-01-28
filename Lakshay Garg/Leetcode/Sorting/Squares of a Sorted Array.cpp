6bd27739301a60b13884c525437986d7700bfc61
Lakshay-2807 <151778286+Lakshay-2807@users.noreply.github.com>
2025-01-28 14:44:52 +0530

    Create Squares of a Sorted Array.cpp

---
+++ Lakshay Garg/Leetcode/Sorting/Squares of a Sorted Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& arr) {
+        for(int i=0;i<arr.size();i++){
+            arr[i]=arr[i]*arr[i];
+        }
+        sort(arr.begin(),arr.end());
+        return arr;
+    }
+};
