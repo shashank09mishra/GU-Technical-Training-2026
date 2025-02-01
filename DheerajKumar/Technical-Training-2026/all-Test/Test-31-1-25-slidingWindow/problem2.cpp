0dcbc0bba1f25d914a1505ba1fbc73aa84feec5e
WHitE-TITaN <dk488621@gmail.com>
2025-02-01 23:53:43 +0530

    basic logic for next vowel !

--- DheerajKumar/Technical-Training-2026/all-Test/Test-31-1-25-slidingWindow/problem2.cpp
+++ DheerajKumar/Technical-Training-2026/all-Test/Test-31-1-25-slidingWindow/problem2.cpp
@@ -5,29 +5,30 @@
 
 using namespace std;
 
-class Solution{
-    public:
-        int largestString(string& s){
-            vector<char> mapper = {'a', 'e', 'i', 'o', 'u'};
-            int start = 0, end = 0, pointer = 0, counter = 0, maxLength = INT_MIN;
-
-            while(end < s.size()){
-                if(s[end] != mapper[pointer]){
-                    pointer++;
-                }
-                if(pointer < 5){
-                    if(s[end] != mapper[pointer]){
-                        start = end;
-                    }
-                }
-                if(counter == 5){
-                    maxLength = max(maxLength, end - start);
-                }
-                end++;
-            }
-            return maxLength == INT_MIN ? 0 : maxLength;
+class Solution {
+public:
+    int longestBeautifulSubstring(string word) {
+        int start = 0, end = 1, maximumLength = 0, counter = 0, currentAlphabet = 0;
+        vector<int> mapper = {'a' - 'a', 'e' - 'a', 'i' - 'a', 'o' - 'a', 'u' - 'a'};
+        
+        while(word[start] - 'a' != 0){
+            start++;
         }
+        cout<<"\nsize of word = "<<word.size();
+        cout<<"\nfirst a is at -> "<<start;
+
+        end = start;
+
+        while(end < word.size()){
+            if(word[end] - 'a' == currentAlphabet){
+
+            }
+            end++;
+        }
+        return maximumLength;
+    }
 };
+
 
 int main(){
     Solution test;
@@ -36,7 +37,7 @@ int main(){
     cout<<"enetr the string ->> ";
     getline(cin, s);
 
-    cout<<"the maximum Lenght -> "<<test.largestString(s);
+    cout<<"the maximum Lenght -> "<<test.longestBeautifulSubstring(s);
 }
 
 
