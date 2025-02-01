1e718b03dad7c8090ab01aed5d7ca689bd74518e
WHitE-TITaN <dk488621@gmail.com>
2025-02-02 01:02:42 +0530

    corrected code for problem 2

--- DheerajKumar/Technical-Training-2026/all-Test/Test-31-1-25-slidingWindow/problem2.cpp
+++ DheerajKumar/Technical-Training-2026/all-Test/Test-31-1-25-slidingWindow/problem2.cpp
@@ -8,24 +8,23 @@ using namespace std;
 class Solution {
 public:
     int longestBeautifulSubstring(string word) {
-        int start = 0, end = 1, maximumLength = 0, counter = 0, currentAlphabet = 0;
-        vector<int> mapper = {'a' - 'a', 'e' - 'a', 'i' - 'a', 'o' - 'a', 'u' - 'a'};
-        
-        while(word[start] - 'a' != 0){
-            start++;
-        }
-        cout<<"\nsize of word = "<<word.size();
-        cout<<"\nfirst a is at -> "<<start;
+        int start = 0, maxLength = 0, uniqueVowels = 1;
 
-        end = start;
-
-        while(end < word.size()){
-            if(word[end] - 'a' == currentAlphabet){
-
+        for (int end = 1; end < word.size(); end++) {
+            if (word[end] < word[end - 1]) {
+                start = end;
+                uniqueVowels = 1;
+            } 
+            else if (word[end] > word[end - 1]) {  
+                uniqueVowels++;
             }
-            end++;
+
+            if (uniqueVowels == 5) {  
+                maxLength = max(maxLength, end - start + 1);
+            }
         }
-        return maximumLength;
+        
+        return maxLength;
     }
 };
 
