a388f6b10ce65db19183dda04a37ab514cccd018
Lovely Kumari <lovely11r12@gmail.com>
2025-02-05 14:35:31 +0530

    SelectionSort

--- LovelyKumari/Sorting/SelectionSort.java
+++ LovelyKumari/Sorting/SelectionSort.java
@@ -1,16 +1,15 @@
 package LovelyKumari.Sorting;
 import java.util.Scanner;
-
-public class BubbleSort {
-    public static void bubbleSort(int arr[] , int n){
-        for(int i=n-1 ; i>=0 ; i--){
-            for(int j=0; j <= i-1 ; j++){
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j+1];
-                    arr[j+1] = arr[j];
-                    arr[j] = temp;
-                }
+public class SelectionSort {
+    public static void selectionSort(int [] arr , int n){
+        for(int i=0 ; i < n-2; i++){
+            int mini=i;
+            for(int j=i ; j<=n-1 ; j++){
+                if(arr[j] < arr[mini]) mini=j;
             }
+            int temp=arr[mini];
+            arr[mini]=arr[i];
+            arr[i]= temp;
         }
     }
     public static void main(String[] args) {
@@ -23,10 +22,13 @@ public class BubbleSort {
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-        bubbleSort(arr , n);
+
+        selectionSort(arr , n);
+
         System.out.print("Sorted array: ");
         for(int num : arr){
             System.out.print(num + " ");
         }
-    } 
+    }
 }
+
