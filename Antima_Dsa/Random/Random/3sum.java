b22b1b7cfacef60f17fe09d20cd91e58462cfe35
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-02-10 19:38:18 +0530

    Update 3sum.java

--- Antima_Dsa/Random/Random/3sum.java
+++ Antima_Dsa/Random/Random/3sum.java
@@ -10,10 +10,8 @@ public class ThreeSum {
 
         // Step 2: Iterate over the array
         for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate values to avoid duplicate triplets
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            // Step 3: Use two pointers to find the other two elements
             int left = i + 1;
             int right = nums.length - 1;
 
@@ -21,10 +19,8 @@ public class ThreeSum {
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if (sum == 0) {
-                    // Found a triplet
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    // Skip duplicate elements
                     while (left < right && nums[left] == nums[left + 1]) left++;
                     while (left < right && nums[right] == nums[right - 1]) right--;
 
