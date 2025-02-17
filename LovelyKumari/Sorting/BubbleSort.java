c3189bb7ab6617d8bd473530856e9cc3da44921e
Lovely Kumari <lovely11r12@gmail.com>
2025-02-17 13:58:44 +0530

    Backtracking

--- LovelyKumari/Sorting/BubbleSort.java
+++ LovelyKumari/Sorting/BubbleSort.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 public class BubbleSort {
     public static void bubbleSort(int arr[] , int n){
         for(int i=n-1 ; i>=0 ; i--){
+            int didSwap=0;
             for(int j=0; j <= i-1 ; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j+1];
                     arr[j+1] = arr[j];
                     arr[j] = temp;
+                    didSwap=1;
                 }
             }
+            if(didSwap == 0) break ;
         }
     }
     public static void main(String[] args) {
