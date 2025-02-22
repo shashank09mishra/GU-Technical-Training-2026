ec0e93c7356bc632b935ba4c609887d47d8d9b38
amansingh989 <amangis989@gmail.com>
2025-02-22 15:31:33 +0530

    problem1.java

--- AmanRajSingh/problem1.java
+++ AmanRajSingh/problem1.java
@@ -1,38 +1,33 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int days) {
-        int mincap= 0;
-        int maxcap = 0;
+    public long repairCars(int[] ranks, int cars) {
 
-        for(int weight:weights){
-            mincap = Math.max(mincap , weight);
-            maxcap += weight;
+        long maxRank = 0;
+        for(int r:ranks){
+            if(r>maxRank){
+                maxRank = r;
+            }
         }
-        int start = mincap;
-        int end = maxcap;
-        int result = 0;
-        while(start<end){
-            int mid = start +(end-start) / 2;
-            if(canShip(weights,days,mid)){
-                end = mid;
+
+        long start = 0, end = (long) maxRank *cars *cars;
+        long ans = 0;
+        while(start<=end){
+            long mid = start +(end-start) / 2;
+            if(canRepair(ranks,mid,cars)){
+                ans = mid;
+                end = mid - 1;
             }
             else{
                 start = mid + 1;
             }
         }
-        return start;
+        return ans;
     }
-    private boolean canShip(int[]weights, int days, int capacity){
-        int currweight = 0;
-        int D = 1;
-        
-        for(int weight:weights){
-            if(currweight + weight > capacity){
-                D++;
-                currweight = 0;
-            }
-            currweight += weight;
+    private boolean canRepair(int[] ranks, long mid,int cars){
+        long totalrepair = 0;
+        for(int r : ranks){
+            totalrepair += (long) Math.sqrt(mid / r);
         }
-        return D <= days;
+    
+    return totalrepair >= cars;
     }
-
 }
